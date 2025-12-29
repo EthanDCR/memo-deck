f692ba12b461d192348845c5c1963c95f35f6829
Ethan <clinteth.dev@gmail.com>
2025-12-28 19:04:28 -0600

    started logic/UI for studying cards

--- wails/frontend/src/pages/StudyPage.tsx
+++ wails/frontend/src/pages/StudyPage.tsx
@@ -2,10 +2,15 @@ import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import { GetDeck } from "../../wailsjs/go/main/App"
 
+type Side = 'front' | 'back'
+
 export default function StudyPage() {
   const { deckName } = useParams()
   const [deck, setDeck] = useState<any>(null)
   const [loading, setLoading] = useState(true)
+  //card state
+  const [cardIndex, setCardIndex] = useState<number>(0)
+  const [cardSide, setCardSide] = useState<Side>('front')
 
   useEffect(() => {
     const loadDeck = async () => {
@@ -23,6 +28,10 @@ export default function StudyPage() {
     return <div>Loading deck...</div>
   }
 
+  //state to control index
+  //no map, just show deck[0]?.front and then button calls function moves to deck at i to back -> deck[0]?.back
+  // then button -> function that changes state of deck to i + 1 .front
+
   return (
     <div>
       <div>
@@ -30,16 +39,12 @@ export default function StudyPage() {
         <p>Flashcards: {deck?.flashCards?.length || 0}</p>
       </div>
 
-
       <div>
-        {!loading && deck?.flashCards?.map((card: any, i: number) => (
-          <div key={i}>
-            <div>front: {card.front}</div>
-            <div>back: {card.back}</div>
-          </div>
-        ))}
-      </div>
+        {!loading &&
+          <div>{deck?.flashCards?.[cardIndex]?.[cardSide]}</div>
+        }
 
+      </div>
 
     </div>
 
