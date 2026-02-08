d2942b727bea2cf0c0f14552e1008ececc325ac2
Ethan <clinteth.dev@gmail.com>
2026-02-08 17:43:20 -0600

    mini sra algorithm for cramming is done

--- wails/stateMachine.go
+++ wails/stateMachine.go
@@ -5,14 +5,13 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"strconv"
+
+	"github.com/wailsapp/wails/v2/pkg/menu/keys"
 )
 
 //after the stuff below lets do some type of progress bar and mesure how well
 //someone knows their deck
-//
-//get length of flashcards(deck)
-//then basically based on n (the action) move the current card to current
-//index + n (action) then load deck client side
 
 type ClientObject struct {
 	DeckName string `json:"deckId"`
@@ -32,10 +31,27 @@ func (*App) UpdateState(input string) (string, error) {
 
 	fmt.Printf("Updating state for %s\n action: %s\n cardId: %s\n", obj.DeckName, obj.Action, obj.CardId)
 
+	var action int
+	switch obj.Action {
+	case "again":
+		action = 0
+		break
+	case "hard":
+		action = 2
+		break
+	case "good":
+		action = 5
+		break
+	case "easy":
+		action = 10
+		break
+	}
+
 	configDir, err := os.UserConfigDir()
 	if err != nil {
 		return fmt.Sprintf("Error: %v/n", err), err
 	}
+
 	memo := "memoDeck"
 	fullDeckName := fmt.Sprintf("%s.json", obj.DeckName)
 	fullFilePath := filepath.Join(configDir, memo, fullDeckName)
@@ -49,14 +65,41 @@ func (*App) UpdateState(input string) (string, error) {
 	var deck = Deck{}
 	json.Unmarshal(deckBytes, &deck)
 
-	// need to loop through deck (the deck from the file on user filesystem)
-	// once deck.flashcards.id == obj.cardid then index := i
-	var currentIndex int
-	for i, card := range deck.FlashCards {
-		if card.ID == obj.CardId {
-			currentIndex = i
-		}
+	currentIndex, err := strconv.Atoi(obj.Index)
+	if err != nil {
+		return "error converting string from client into int (index): ", err
 	}
 
-	return "", nil
+	var newSlice []FlashCard
+	temp := deck.FlashCards[currentIndex]
+
+	newSlice = deck.FlashCards[:currentIndex]
+	newSlice = append(newSlice, deck.FlashCards[currentIndex+1:]...)
+	indexToInt, err := strconv.Atoi(obj.Index)
+	targetIndex := indexToInt + action
+
+	if targetIndex > len(newSlice) {
+		targetIndex = len(newSlice)
+	}
+
+	left := newSlice[:targetIndex]
+	right := newSlice[targetIndex:]
+	var finalSlice []FlashCard
+	finalSlice = left
+	finalSlice = append(finalSlice, temp)
+	finalSlice = append(finalSlice, right...)
+
+	deck.FlashCards = finalSlice
+
+	updatedDeckBytes, err := json.MarshalIndent(deck, "", "  ")
+	if err != nil {
+		return "Error encoding deck to JSON", err
+	}
+
+	err = os.WriteFile(fullFilePath, updatedDeckBytes, 0644)
+	if err != nil {
+		return "error updating file", err
+	}
+
+	return "file updated successfully ", nil
 }
